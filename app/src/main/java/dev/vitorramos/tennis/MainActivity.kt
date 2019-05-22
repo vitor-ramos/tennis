b5e2c7a39b3d4791e4703647dc8ee081c2b2dcea
Vitor Ramos <hey@vitorramos.dev>
2019-05-22 09:34:02 -0300

    MainActivity visual changes

--- app/src/main/java/dev/vitorramos/tennis/MainActivity.kt
+++ app/src/main/java/dev/vitorramos/tennis/MainActivity.kt
@@ -1,7 +1,9 @@
 package dev.vitorramos.tennis
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,15 +11,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        start_match.setOnClickListener {
-//            with(Intent(this, GameActivity::class.java)) {
-//                putExtra(EXTRA_FIELD_HOST, host_name.text?.toString() ?: getString(R.string.you))
-//                putExtra(EXTRA_FIELD_GUEST, guest_name.text?.toString() ?: getString(R.string.guest))
-//                putExtra(EXTRA_FIELD_GAMES, games_count.text?.toString()?.toInt() ?: 3)
-//                putExtra(EXTRA_FIELD_SETS, sets_count.text?.toString()?.toInt() ?: 2)
-//                startActivity(this)
-//            }
-//        }
+        start_match.setOnClickListener {
+            with(Intent(this, GameActivity::class.java)) {
+                putExtra(EXTRA_FIELD_HOST, host_name.text?.toString() ?: getString(R.string.you))
+                putExtra(EXTRA_FIELD_GUEST, guest_name.text?.toString() ?: getString(R.string.guest))
+                putExtra(EXTRA_FIELD_GAMES, games_count.text?.toString()?.toInt() ?: 3)
+                putExtra(EXTRA_FIELD_SETS, sets_count.text?.toString()?.toInt() ?: 2)
+                startActivity(this)
+            }
+        }
     }
 
     companion object {
