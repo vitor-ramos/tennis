5b53d10c7e46866d4223975af723a31b5b6399e2
Vitor Ramos <hey@vitorramos.dev>
2019-07-19 15:58:25 -0300

    HomeActivity added

---
+++ app/src/main/java/dev/vitorramos/tennis/view/HomeActivity.kt
@@ -0,0 +1,23 @@
+package dev.vitorramos.tennis.view
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dev.vitorramos.tennis.R
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        bt_home_start_match.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        bt_home_history.setOnClickListener {
+            startActivity(Intent(this, HistoryActivity::class.java))
+        }
+    }
+}
