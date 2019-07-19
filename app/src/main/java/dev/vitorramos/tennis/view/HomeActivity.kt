02cecd2a91ddacb64b8218dc431752914ea42041
Vitor Ramos <hey@vitorramos.dev>
2019-07-19 18:39:30 -0300

    Some additions to home page

--- app/src/main/java/dev/vitorramos/tennis/view/HomeActivity.kt
+++ app/src/main/java/dev/vitorramos/tennis/view/HomeActivity.kt
@@ -3,12 +3,13 @@ package dev.vitorramos.tennis.view
 import android.content.Intent
 import android.os.Bundle
 import android.view.View.VISIBLE
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import dev.vitorramos.tennis.PREF_FIELD_MATCH_ID
-import dev.vitorramos.tennis.R
 import dev.vitorramos.tennis.PREF_FILE_NAME
+import dev.vitorramos.tennis.R
 import dev.vitorramos.tennis.viewModel.HomeViewModel
 import kotlinx.android.synthetic.main.activity_home.*
 
@@ -19,9 +20,30 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         val viewModel = ViewModelProviders.of(this)[HomeViewModel::class.java]
+
+        viewModel.onGoingMatch.observe(this, Observer { isThereAGame ->
+            if (isThereAGame) {
+                bt_home_start_match.setOnClickListener {
+                    AlertDialog.Builder(this).apply {
+                        setCancelable(false)
+                        setTitle("Partida em Andamento")
+                        setMessage("Por favor, encerre a partida atual antes de iniciar outra")
+                        setPositiveButton("OK") { dialog, _ ->
+                            dialog.dismiss()
+                        }
+                        show()
+                    }
+                }
+            } else {
+                bt_home_start_match.setOnClickListener {
+                    startActivity(Intent(this, StartMatchActivity::class.java))
+                }
+            }
+        })
+
         getSharedPreferences(PREF_FILE_NAME, MODE_PRIVATE).getLong(PREF_FIELD_MATCH_ID, -1).also { matchId ->
-            if(matchId != -1L) viewModel.currentMatch(matchId).observe(this, Observer {
-                if(it != null) {
+            if (matchId != -1L) viewModel.currentMatch(matchId).observe(this, Observer {
+                if (it != null) {
                     home_current_host_name.text = it.hostName
                     home_current_host_points_label.text = it.hostPoints.toString()
                     home_current_host_games_label.text = it.hostGames.toString()
@@ -36,10 +58,6 @@ class HomeActivity : AppCompatActivity() {
                     home_current_guest_layout.visibility = VISIBLE
                 }
             })
-        }
-
-        bt_home_start_match.setOnClickListener {
-            startActivity(Intent(this, StartMatchActivity::class.java))
         }
 
         bt_home_history.setOnClickListener {
--- app/src/main/java/dev/vitorramos/tennis/viewModel/HomeViewModel.kt
+++ app/src/main/java/dev/vitorramos/tennis/viewModel/HomeViewModel.kt
@@ -1,9 +1,18 @@
 package dev.vitorramos.tennis.viewModel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dev.vitorramos.tennis.entity.MatchEntity
 import dev.vitorramos.tennis.repository.Repository
 
 class HomeViewModel : ViewModel() {
-    fun currentMatch(matchId: Long) = Repository.it?.getMatch(matchId) ?: MutableLiveData()
+    val onGoingMatch = MutableLiveData<Boolean>().apply {
+        postValue(false)
+    }
+
+    fun currentMatch(matchId: Long): LiveData<MatchEntity?> {
+        onGoingMatch.postValue(true)
+        return Repository.it?.getMatch(matchId) ?: MutableLiveData()
+    }
 }
