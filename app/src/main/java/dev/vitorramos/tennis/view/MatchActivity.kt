8247a0f97901a25901a0455e2e520a31b12fddd5
Vitor Ramos <hey@vitorramos.dev>
2019-09-04 19:08:21 -0300

    Match activity ids renamed

--- app/src/main/java/dev/vitorramos/tennis/view/MatchActivity.kt
+++ app/src/main/java/dev/vitorramos/tennis/view/MatchActivity.kt
@@ -13,9 +13,6 @@ import dev.vitorramos.tennis.viewModel.MatchViewModel
 import kotlinx.android.synthetic.main.activity_match.*
 
 class MatchActivity : AppCompatActivity() {
-    private var hostName = ""
-    private var guestName = ""
-
     private var viewModel: MatchViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,34 +20,12 @@ class MatchActivity : AppCompatActivity() {
         setContentView(R.layout.activity_match)
 
         viewModel = ViewModelProviders.of(this).get(MatchViewModel::class.java)
+        prepareObservers()
 
-        intent.extras?.getLong(EXTRA_MATCH_ID)?.let { matchId ->
-            viewModel?.matchId = matchId
-            viewModel?.currentMatch?.observe(this, Observer {
-                it?.let {
-                    hostName = if (it.hostName != "") it.hostName else getString(R.string.you)
-                    guestName =
-                        if (it.guestName != "") it.guestName else getString(R.string.guest_name)
-
-                    component_match_started.text = getFormattedDate(resources, it.started)
-
-                    component_match_host_name.text = hostName
-                    component_match_host_points.text = getFormattedPoints(it.hostPoints)
-                    component_match_host_games.text = it.hostGames.toString()
-                    component_match_host_sets.text = it.hostSets.toString()
-
-                    component_match_guest_name.text = guestName
-                    component_match_guest_points.text = getFormattedPoints(it.guestPoints)
-                    component_match_guest_games.text = it.guestGames.toString()
-                    component_match_guest_sets.text = it.guestSets.toString()
-                }
-            })
-        }
-
-        component_match_host_button.setOnClickListener {
+        bt_match_host_button.setOnClickListener {
             viewModel?.addHostPoint()
         }
-        component_match_guest_button.setOnClickListener {
+        bt_match_guest_button.setOnClickListener {
             viewModel?.addGuestPoint()
         }
     }
@@ -66,6 +41,31 @@ class MatchActivity : AppCompatActivity() {
             finish()
             true
         } else false
+    }
+
+    private fun prepareObservers() {
+        intent.extras?.getLong(EXTRA_MATCH_ID)?.let { matchId ->
+            viewModel?.matchId = matchId
+            viewModel?.currentMatch?.observe(this, Observer {
+                it?.let {
+                    val hostName = if (it.hostName != "") it.hostName else getString(R.string.you)
+                    val guestName =
+                        if (it.guestName != "") it.guestName else getString(R.string.guest_name)
+
+                    tv_match_started.text = getFormattedDate(resources, it.started)
+
+                    tv_match_host_name.text = hostName
+                    tv_match_host_points.text = getFormattedPoints(it.hostPoints)
+                    tv_match_host_games.text = it.hostGames.toString()
+                    tv_match_host_sets.text = it.hostSets.toString()
+
+                    tv_match_guest_name.text = guestName
+                    tv_match_guest_points.text = getFormattedPoints(it.guestPoints)
+                    tv_match_guest_games.text = it.guestGames.toString()
+                    tv_match_guest_sets.text = it.guestSets.toString()
+                }
+            })
+        }
     }
 
     companion object {
